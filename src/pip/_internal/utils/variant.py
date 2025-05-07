c88494e8f8b6df0e9881a8db5e018dccb1d673e3
Michał Górny <mgorny@gentoo.org>
2025-05-07 15:36:43 +0200

    Remove unused `read_provider_priority_from_pip_config()`

--- src/pip/_internal/utils/variant.py
+++ src/pip/_internal/utils/variant.py
@@ -12,29 +12,6 @@ from pip._internal.exceptions import ConfigurationError, PipError
 logger = logging.getLogger(__name__)
 
 
-def read_provider_priority_from_pip_config() -> dict[str, int]:
-    try:
-        config = Configuration(isolated=False)
-        config.load()
-
-    except PipError:
-        logging.exception("Error while reading PIP configuration")
-        return {}
-
-    try:
-        provider_priority = config.get_value("variantlib.provider_priority")
-
-        if provider_priority is None or not isinstance(provider_priority, str):
-            return {}
-
-        return {item: idx for idx, item in enumerate(provider_priority.split(","))}
-
-    except ConfigurationError:
-        # the user didn't set a special configuration
-        logging.warning("No Variant Provider prioritization was set inside `pip.conf`.")
-        return {}
-
-
 class VariantJson(dict):
     def __hash__(self):
         return hash(tuple(self.get("variants")))
